91d1c2b56e7f5c9ccb4b11e3e8247d9b4e173951
Mayankarjoriya <mayankrajoriya2004@gmail.com>
2026-04-06 09:19:13 +0530

    I added more complex env.py code and if anything wrong or may be reconsider let me know

--- eny.py
+++ eny.py
@@ -4,19 +4,6 @@ class TrafficEnv:
 
 
     def state(self):
-        self.state = {
-            "north_cars": random.randint(1, 20),  # 0-20 random cars
-            "south_cars": random.randint(1,20),
-            "west_cars": random.randint(1,20),
-            "east_cars": random.randint(1,20),
-            "north_wait": 0,
-            "west_wait": 0,
-            "east_wait": 0,
-            "south_wait": 0,
-            "current_green_0": None,  # sab red matlab "none"
-            "current_green_1": None,
-            "ambulance": False
-        }
 
         return self.state
     
@@ -62,9 +49,61 @@ class TrafficEnv:
             self.state["east_cars"] = 0
             self.state["west_cars"] = 0
 
-        
+        elif action == "NE_GREEN":
+            self.state["current_green_0"] = "east"
+            self.state["current_green_1"] = "north" 
 
-        return self.state , Reward
+            # Step 2 Reward
+            Reward = 0.0
+            Reward -= self.state["west_cars"]*0.1
+            Reward -= self.state["south_cars"]*0.1
+           
+
+            # Measure waiting Time
+            self.state["west_wait"] += 10
+            self.state['north_wait'] += 10
+
+            # Empty no. of cars in selected lane
+            self.state["east_cars"] = 0
+            self.state["north_cars"] = 0
+        
+        elif action == "NW_GREEN":
+            self.state["current_green_0"] = "north"
+            self.state["current_green_1"] = "west" 
+
+            # Step 2 Reward
+            Reward = 0.0
+            Reward -= self.state["east_cars"]*0.1
+            Reward -= self.state["south_cars"]*0.1
+           
+
+            # Measure waiting Time
+            self.state["south_wait"] += 10
+            self.state['east_wait'] += 10
+
+            # Empty no. of cars in selected lane
+            self.state["north_cars"] = 0
+            self.state["west_cars"] = 0
+        done = (
+    self.state["north_cars"] == 0 and
+    self.state["south_cars"] == 0 and
+    self.state["east_cars"] == 0 and
+    self.state["west_cars"] == 0
+)
+
+        info = {
+    "total_wait": self.state["north_wait"] + self.state["south_wait"] + 
+                  self.state["east_wait"] + self.state["west_wait"]
+
+}
+        if self.state["rush_hour"] and self.state["rush_hour"] != False:
+            rush_lane= self.state["rush_hour"]
+            self.state[f"{rush_lane}_cars"] += random.randint(1,5)
+        # Ambulance penalty
+        if self.state["ambulance"] and self.state["ambulance_lane"] not in [current_green_0, current_green_1]:
+            Reward -= 5.0  # Max penalty!
+
+        return self.state , Reward, done, info
 
 
     def reset(self, task_id=1):
@@ -81,26 +120,46 @@ class TrafficEnv:
             "south_wait": 0,
             "current_green_0": None,  # sab red matlab "none"
             "current_green_1": None,
-            "ambulance": False
+            "ambulance": False,
+            "ambulance_lane": None,
+            "rush_hour": False,
         }
             
         # medium
         elif task_id == 2:
-
-
-        # Hard
-        elif task_id == 3:  
             self.state = {
-            "north_cars": random.randint(1, 30),  # 0-20 random cars
-            "south_cars": random.randint(1,30),
-            "east_cars": random.randint(1,30),
-            "west_cars": random.randint(1,30),
+            "north_cars": random.randint(10, 20),  # 0-20 random cars
+            "south_cars": random.randint(10,20),
+            "east_cars": random.randint(10,20),
+            "west_cars": random.randint(10,20),
             "north_wait": 0,
             "west_wait": 0,
             "east_wait": 0,
             "south_wait": 0,
             "current_green_0": None,  # sab red matlab "none"
             "current_green_1": None,
-            "ambulance": "north"
+            "ambulance": False,
+            "ambulance_lane": None,
+            "rush_hour": "north",
+        }
+
+
+
+        # Hard
+        elif task_id == 3:  
+            self.state = {
+            "north_cars": random.randint(15, 30),  # 0-20 random cars
+            "south_cars": random.randint(15,30),
+            "east_cars": random.randint(15,30),
+            "west_cars": random.randint(15,30),
+            "north_wait": 0,
+            "west_wait": 0,
+            "east_wait": 0,
+            "south_wait": 0,
+            "current_green_0": None,  # sab red matlab "none"
+            "current_green_1": None,
+            "ambulance": True,
+            "ambulance_lane": "west",
+            "rush_hour": "north",
         }
        
